4dd4692975765ecb3ba24097b21e08a90c68613c
Bálint Kovács <kovacsb@starschema.net>
2024-10-14 12:53:06 +0200

    Reading secrets from .env file
    Your local folder should contain a .env file, containing secrets such as Azure OpenAI deployment names and endpoints. Typically, `.env` files are not commited to git, as they can contain sensitive information.

--- rag.py
+++ rag.py
@@ -1,9 +1,16 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+llm_deployment = os.getenv("AZURE_LLM_DEPLOYMENT")
+azure_api_key = os.getenv("AZURE_OPENAI_API_KEY")
+azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+azure_api_version = os.getenv("API_VERSION")
 
 # bge-base embedding model
 embed_model =  HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-
 Settings.embed_model = embed_model
 
 documents = SimpleDirectoryReader("data").load_data()
