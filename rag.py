b29de38de24c5d101578fe24b9d5882df805ca91
Bálint Kovács <kovacsb@starschema.net>
2024-10-18 12:33:57 +0200

    Created `config.toml` file TOML is a format for config files. It contains tags where we can group variables. We created these for chunking, data, and retrival parameters.
    Saving hyperparameters to a config file is a good practice, because if we want to experiment with different parameters, we can access them in one place. Config files don't contain sensitive information, so they can be stored in the repo.

--- rag.py
+++ rag.py
@@ -8,6 +8,7 @@ from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 from llama_index.llms.azure_openai import AzureOpenAI
 from dotenv import load_dotenv
 import os
+import toml
 
 
 load_dotenv()
@@ -16,6 +17,10 @@ embedding_deployment = os.getenv("AZURE_EMBEDDING_DEPLOYMENT")
 azure_api_key = os.getenv("AZURE_OPENAI_API_KEY")
 azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 azure_api_version = os.getenv("API_VERSION")
+
+config = toml.load("config.toml")
+PERSIST_DIR = config["data"]["persist_dir"]
+SIMILARITY_TOP_K = config["retrieval"]["similarity_top_k"]
 
 # bge-base embedding model
 embed_model= AzureOpenAIEmbedding(
@@ -36,11 +41,10 @@ llm = AzureOpenAI(
 )
 Settings.llm = llm
 
-PERSIST_DIR = "./storage"
 storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
 index = load_index_from_storage(storage_context)
 
-query_engine = index.as_query_engine(similarity_top_k=10)
+query_engine = index.as_query_engine(similarity_top_k=SIMILARITY_TOP_K)
 response = query_engine.query("What did the author do growing up?")
 print(response)
 
