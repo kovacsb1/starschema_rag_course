d22b12f32def2e7ea600ce84886ecd2fa131af94
Bálint Kovács <kovacsb@starschema.net>
2024-10-25 18:24:18 +0200

    Using QDrant VectorDB as storage context
    So far, we have used local disk persistence for our embedding vectors. Now, we use QDrant vector database. QDrant runs on a kubernetes cluster in Azure in our setup. We need to set its URL, port, api key and the collection name we want to create. These are in the `config.toml` file. A vector database creates an efficient index, that can quickly find chunks with the most semantic simiarity. However, this effect is only relevant when we have chunks in the range of millions.
    
    We need to change a couple config parameters: the details of Qdrant access is stored in the .env file(that is local to everyone), because that is sensitive information. The collection name is stored in config file however, as that is not sensitive. We also removed PERSIST_DIR, as we are not saving our embedding vectors on disk.
    
    In ingestion.py, first we create a qdrant client that can connect to the running Qdrant instance. Then we create a `QdrantVectorStore` object, and create a storage context from it. Then we pass this storage context to our `VectorStoreIndex`. We no longer need to persist, as our embeddings are automatically persisted in the vector DB.

--- ingestion.py
+++ ingestion.py
@@ -5,21 +5,27 @@ from llama_index.core import (
 )
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core import StorageContext
+from llama_index.vector_stores.qdrant import QdrantVectorStore
 from dotenv import load_dotenv
 import os
 import toml
+import qdrant_client
 
 load_dotenv()
 embedding_deployment = os.getenv("AZURE_EMBEDDING_DEPLOYMENT")
 azure_api_key = os.getenv("AZURE_OPENAI_API_KEY")
 azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
 azure_api_version = os.getenv("API_VERSION")
+qdrant_url = os.getenv("QDRANT_URL")
+qdrant_port = os.getenv("QDRANT_PORT")
+qdrant_api_key = os.getenv("QDRANT_API_KEY")
 
 config = toml.load("config.toml")
 CHUNK_SIZE = config["chunking"]["chunk_size"]
 CHUNK_OVERLAP = config["chunking"]["chunk_overlap"]
 SOURCE_DIR = config["data"]["source_dir"]
-PERSIST_DIR = config["data"]["persist_dir"]
+QDRANT_COLLECTION_NAME = config["vectordb"]["collection_name"]
 
 embed_model= AzureOpenAIEmbedding(
     model="text-embedding-ada-002",
@@ -35,6 +41,17 @@ splitter = SentenceSplitter(
     chunk_overlap=CHUNK_OVERLAP,
 )
 
+client = qdrant_client.QdrantClient(
+    url=qdrant_url,
+    port=qdrant_port,
+    api_key=qdrant_api_key,
+)
+
 documents = SimpleDirectoryReader(SOURCE_DIR, recursive=True).load_data()
-index = VectorStoreIndex.from_documents(documents, transformations=[splitter])
-index.storage_context.persist(persist_dir=PERSIST_DIR)
+vector_store = QdrantVectorStore(client=client, collection_name=QDRANT_COLLECTION_NAME)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+index = VectorStoreIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+    transformations=[splitter]
+)
